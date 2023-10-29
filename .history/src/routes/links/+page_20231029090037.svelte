b501ef25b9cb0ea02eaf4627a55a1c93c3014f2a
Freezler <r.l.devries@live.nl>
2023-10-29 09:01:04 +0100

    dew

--- .history/src/routes/links/+page_20231029090037.svelte
+++ .history/src/routes/links/+page_20231029090037.svelte
@@ -2,6 +2,7 @@
 	import { onDestroy, onMount } from 'svelte';
 	onMount(() => console.log('Mounted Links page'));
 	onDestroy(() => console.log('Unmounted Links page'));
+	let email =	'r.l.devries@live.nl';
 	import Blob from '../components/scrollBlob.svelte';
 </script>
 <div class="flex flex-col items-center justify-center">
