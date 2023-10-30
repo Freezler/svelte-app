dd3eb8aa3de679c57a5f83b08d4d9513b06f5e47
Freezler <r.l.devries@live.nl>
2023-10-30 20:13:41 +0100

    add content

---
+++ .history/src/routes/links/+page_20231030193036.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { onDestroy, onMount } from 'svelte';
+	onMount(() => console.log('Mounted Links page'));
+	onDestroy(() => console.log('Unmounted Links page'));
+	import ScrollBlob from '../../components/scrollBlob.svelte';
+</script>
+<main class="flex flex-col w-screen h-screen bg-gradient-to-tr from-astral-900 via-gray-900 to-astral-900">
+<div class="flex flex-col items-center justify-center">
+<ScrollBlob/>
+</div>
+</main>
