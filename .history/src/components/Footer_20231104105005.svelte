dc9dbb3567c053ca50c92afeb9e5936d9566c159
Freezler <r.l.devries@live.nl>
2023-11-04 10:51:54 +0100

    rm github icon

--- .history/src/components/Footer_20231104105005.svelte
+++ .history/src/components/Footer_20231104105005.svelte
@@ -4,6 +4,6 @@
 <div class="bg-astral-900 bg-opacity-50 fixed w-screen bottom-0 h-20 flex items-center p-0 m-0">
   <div class="flex w-full items-center justify-around">
 	<!-- GitHub icon -->
-	<small class="text-white italic">Randy de vries &copy; {new Date().getFullYear()}</small>
+	<small></small>
   </div>
 </div>
