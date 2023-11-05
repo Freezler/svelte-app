f5039ace1bd2886147b7d6b6294ffa5cff0f3058
Freezler <r.l.devries@live.nl>
2023-11-05 10:39:02 +0100

    try

--- .history/src/components/Hero_20231105103219.svelte
+++ .history/src/components/Hero_20231105103219.svelte
@@ -9,20 +9,20 @@
 </script>
 
 
-<section class="mt-[10%] sm:mt-[3%] md:mt-[3%] lg:mt-[3%] xl:mt-[0px] w-full md:w-[60%] mx-auto flex h-[320px] flex-col items-center justify-center bg-slate-400 bg-opacity-30 bg-cover text-center" id="home">
+<section class="mt-[10%] sm:mt-[5%] md:mt-[10%] lg:mt-[5%] xl:mt-[0px] w-full md:w-[60%] mx-auto flex h-[300px] flex-col items-center justify-center bg-slate-400 bg-opacity-30 bg-cover text-center" id="home">
   <div class="flex flex-col gap-2">
 	<div class="animate-slow-fade-in flex flex-col items-center justify-center">
 	  <Avatar bgColor="hsla(234,100%,59%,0.3)" textColor="white" name="Randy de vries" src="https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Blank&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue03&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale" size="180px" />
 	</div>
 	<div>
-	  <h3 class="text-xl font-[900] text-cyan-300"><span class="text-purpleone-200 text-[16px]"><small>hi I'am, </small></span>RANDY</h3>
-	  <p>Welcome to my portfolio made with Svelte & TailwindCSS</p>
+	  <h1><span>hi I'am, </span>RANDY</h1>
+	  <p>responsieve hero sectie!!</p>
 	</div>
-	<div class="font-[500] ">
-	  <button class="text-[12px] bg-purpleone-200 py-1 px-2 rounded-md hover:text-white hover:bg-black"  type="button">
+	<div>
+	  <button class="bg-purpleone-200 py-1 px-2 rounded-md hover:text-white hover:bg-black"  type="button">
 		INFO
 	  </button>
-	  <button class="text-[12px] bg-purpleone-200 py-1 px-2 rounded-md hover:text-white hover:bg-black" on:click={handleContactButtonClick} type="button">
+	  <button class="bg-purpleone-200 py-1 px-2 rounded-md hover:text-white hover:bg-black" on:click={handleContactButtonClick} type="button">
 		CONTACT
 	  </button>
 	</div>
--- .history/src/components/Hero_20231105103840.svelte
+++ .history/src/components/Hero_20231105103840.svelte
@@ -9,7 +9,7 @@
 </script>
 
 
-<section class="mt-[10%] sm:mt-[3%] md:mt-[3%] lg:mt-[3%] xl:mt-[0px] w-full md:w-[60%] mx-auto flex h-[320px] flex-col items-center justify-center bg-slate-400 bg-opacity-30 bg-cover text-center" id="home">
+<section class="mt-[10%] sm:mt-[5%] md:mt-[10%] lg:mt-[3%] xl:mt-[0px] w-full md:w-[60%] mx-auto flex h-[320px] flex-col items-center justify-center bg-slate-400 bg-opacity-30 bg-cover text-center" id="home">
   <div class="flex flex-col gap-2">
 	<div class="animate-slow-fade-in flex flex-col items-center justify-center">
 	  <Avatar bgColor="hsla(234,100%,59%,0.3)" textColor="white" name="Randy de vries" src="https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Blank&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue03&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale" size="180px" />
