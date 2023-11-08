e1638bf1e44ca49701904fda325ae260e49203cb
Freezler <r.l.devries@live.nl>
2023-11-09 00:25:56 +0100

    ch3ck

---
+++ .history/src/components/Accordion_20231108235803.svelte
@@ -0,0 +1,77 @@
+<script>
+	let panels = [
+		{
+			title: 'HTML',
+			content:
+				'HTML is the standard markup language for documents designed to be displayed in a web browser.'
+		},
+		{ title: 'CSS', content: 'CSS is used to define styles for your web pages' },
+		{
+			title: 'JavaScript',
+			content: 'JavaScript is used to add interactivity to your web pages'
+		},
+		{
+			title: 'Svelte and SvelteKit',
+			content: 'Svelte and SvelteKit are both JavaScript frameworks'
+		},
+		{ title: 'Tailwind CSS', content: 'Tailwind CSS is a utility-first CSS framework' }
+	];
+
+	let expandedPanel = null;
+
+	function togglePanel(panel) {
+		if (expandedPanel === panel) {
+			expandedPanel = null;
+		} else {
+			expandedPanel = panel;
+		}
+	}
+</script>
+
+<div class="bg-transparent animate-slow-slide-up z-60 wrapper max-w-50 mx-auto mt-[48px] w-[60%] text-center rounded-sm p-8">
+	<div class="flex flex-col space-y-1  rounded-md border-[1px] border-astral-900">
+		{#each panels as panel}
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<div
+				class="relative isolate animate-fast-fade-in overflow-hidden"
+				on:click={() => togglePanel(panel)}
+				on:keydown={() => togglePanel(panel)}
+				aria-roledescription="accordion"
+			>
+				<div class="" aria-expanded={panel === expandedPanel}>
+					<!-- svelte-ignore empty-block -->
+					{#if panel === expandedPanel}{/if}
+					<div
+						class="relative isolate grid place-items-center bg-[rgba(0,0,0,0.2)] p-2 font-display font-bold text-white"
+					>
+						{panel.title}
+					</div>
+				</div>
+				{#if expandedPanel === panel}
+					<div
+						class="z-20 animate-slow-fade-in bg-astral-900 bg-opacity-70 px-6 py-0"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="icon icon-tabler icon-tabler-chevron-up mx-auto animate-pulse"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="white"
+							fill="rgba(39, 68, 82, 100%)"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						>
+							<path stroke="none" d="M0 0h24v24H0z" fill="transparent" />
+							<polyline points="6 15 12 9 18 15" />
+						</svg>
+						<p class="text-white pb-3">
+							{panel.content}
+						</p>
+					</div>
+				{/if}
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ .history/src/components/Hero_20231109001715.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { sendEmail } from '$lib/index.js';
+	import Avatar from 'svelte-avatar';
+  
+	function handleContactButtonClick() {
+	  // Call the sendEmail function here
+	  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
+	}
+</script>
+
+<section class="absolute top-[30%] xxs:top-[20%] xs:top-[30%] sm:top-[40%] md:top-[30%] xl:top-[20%] mx-auto w-[100%] md:w-[100%] filter contrast-400 z-100 -translate-y-[190px] sm:-translate-y-[190px] md:-translate-y-[220px]  flex flex-col justify-center items-center text-center h-full">
+	<div class="relative flex flex-col items-center justify-center">
+		<Avatar bgColor="goldenrodyellow" textColor="white" name="Randy de vries" src="https://avataaars.io/?avatarStyle=Circle&topType=NoHair&accessoriesType=Blank&facialHairType=Blank&clotheType=Hoodie&clotheColor=Blue03&eyeType=Default&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale" size="200px" />
+		<div class="bg-opacity-50 bg-purpleone-900 rounded-md flex flex-col items-center justify-center mt-[24px]">
+			<h3 class="font-[500] text-2xl text-blueone-200">
+				<span class="font-sans text-redone-200">hi I am,</span> RANDY
+			</h3>
+			<p class="bg-opacity-20 bg-purpleone-700 rounded-md text-purpleone-100 text-shadow-[3px_3px_5px_rgba(22,22,22,0.9)] text-center w-[80%]">Welcome to my web project made with Svelte & TailwindCSS</p>
+		</div>
+		<div class="font-[500] mt-4 pb-4">
+			<button class="text-[clamp(12px,2vw,20px)] text-purple-700 bg-purplethree-200 py-1 px-2 md:py-2 md:px-6 rounded-md hover:text-white hover:bg-purpleone-400 transition-colors duration-300 hover:transition-colors hover:transition-duration-300 ease-in-out" type="button">
+				INFO
+			</button>
+			<button class="text-[clamp(12px,2vw,20px)] text-purple-700 bg-purplethree-200 py-1 px-2 md:py-2 md:px-6 rounded-md hover:text-white hover:bg-purpleone-400 transition-colors duration-300 hover:transition-colors hover:transition-duration-300 ease-in-out" on:click={sendEmail}  type="button">
+				CONTACT
+			</button>
+		</div>
+	</div>
+</section>
