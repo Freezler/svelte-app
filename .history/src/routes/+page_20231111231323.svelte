2ffa7241aa76ad471337cfe74b46fc57743e10a6
Freezler <r.l.devries@live.nl>
2023-11-11 23:35:13 +0100

    addede

---
+++ .history/src/routes/+page_20231111231323.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onDestroy, onMount } from 'svelte';
+    import MapContact from '../components/Header.svelte';
+    import Cards from '../components/Cards.svelte';
+	import Header from '../components/Header.svelte';
+
+
+    onMount(() => console.log('Mounted home page'));
+    onDestroy(() => console.log('Unmounted home page'));
+</script>
+
+<main class="scroll-smooth flex flex-col w-[100%] h-screen items-center overflow-scroll justify-center bg-gradient-to-b from-astral-200 via-gray-200 to-astral-400 bg-cover ">
+    <div class="scroll-smooth relative grid grid-cols-1 h-[100%] w-screen overflow-scroll  bg-cover bg-center bg-no-repeat pt-[150px] md:pt-20 xl:pt-20">
+		<Header />
+        <div class="container pb-2 ">
+            <div class="flex flex-col items-center justify-center w-screen h-[5rem] bg-red-700">ha</div>
+        </div>
+		<Cards />
+		
+	</div>
+    
+</main>
+
