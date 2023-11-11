ea38f05d8a6aed0cc39bb9f9d8f61a7008af2f79
Freezler <r.l.devries@live.nl>
2023-11-11 15:42:58 +0100

    abba

---
+++ .history/src/components/MapContact_20231111153844.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+	<div class="transition-all animate-slow-slide-in font-Inter absolute top-6 antialiased	 text-astral-400 h-[160px] contrast-100 0 w-screen  bg-[url('https://source.unsplash.com/1080x1080/?development')] bg-cover z-0 skew-y-2 origin-top-right">
+		<h1 class="animate-fade-in mt-[2.2rem] mx-16 text-6xl font-bold font-display text-shadow-[2px_1px_1px_hsla(255,100%,29%,0.9)] -skew-y-6">Randy de Vries</h1>
+		<p class=" animate-slow-fade-in text-sm text-end mx-4 -skew-y-3 font-thin text-astral-200 text-shadow-[1px_1px_12px_hsla(25,100%,9%,0.9)] ">javaScript | Svelte | TailwindCSS | junior developer</p>
+	</div>
+
+
