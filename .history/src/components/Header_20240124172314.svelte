53c2b690aebd3b0508527142ea76158d6fbbd32e
Freezler <r.l.devries@live.nl>
2024-01-24 17:40:59 +0100

    change landing

---
+++ .history/src/components/Header_20240124172314.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+</script>
+
+<div
+	class=" object-fit absolute top-[15svh] md:top-[8svh] z-0 flex h-[150px] w-screen skew-y-0 animate-slow-fade-in flex-col  bg-greyone-900/20 rounded-md bg-clip-padding backdrop-filter backdrop-blur-[2px] bg-opacity-40 
+	xs:text-left md:h-[150px]"
+>
+	<h1
+		class="px-8 mx-auto py-2 m-0 -skew-y-0 animate-fast-fade-in uppercase font-sans max-w-[100%]
+		text-[clamp(1.8rem,3vw,4rem)] font-bold tracking-[5px] text-shadow-[2px_5px_5px_hsla(2,50%,9%,0.9)]"
+	>
+		Randy de Vries
+	</h1>
+	<p
+		class=" box-border font-mono mx-12 -skew-y-0 animate-slow-fade-in border-t-[0.5px] pt-3  text-xs font-normal tracking-[5px] text-astral-100text-center m-0 leading-[18px]
+		text-[clamp(.8rem,3vw,1.2rem)]  sm:text-left"
+	>
+		|javaScript|Svelte|<br>|<span class="text-teal-900 font-thin">Junior|Web|Developer</span>|
+	</p>
+</div>
---
+++ .history/src/components/Header_20240124173852.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+</script>
+
+<div
+	class=" object-fit absolute top-[10svh] md:top-[9svh] z-0 flex h-[150px] w-screen skew-y-0 animate-slow-fade-in flex-col  bg-greyone-900/20 rounded-md bg-clip-padding backdrop-filter backdrop-blur-[2px] bg-opacity-40 
+	xs:text-left md:h-[178px]"
+>
+	<h1
+		class="px-8 mx-auto py-4 md:py-0 p-0 -skew-y-0 animate-fast-fade-in uppercase font-sans max-w-[100%]
+		text-[clamp(1.8rem,5vw,6rem)] font-bold tracking-[5px] text-shadow-[2px_5px_5px_hsla(2,50%,9%,0.9)]"
+	>
+		Randy de Vries
+	</h1>
+	<p
+		class=" box-border font-mono mx-12 -skew-y-0 animate-slow-fade-in border-t-[0.5px] pt-3   font-normal tracking-[5px] text-astral-100text-center m-0 leading-[18px]
+		text-[clamp(.5rem,2vw,1rem)]  sm:text-right md:text-center "
+	>
+		|javaScript|Svelte|<br>|<span class="text-teal-300 font-semibold">Junior Web Developer</span>|
+	</p>
+</div>
