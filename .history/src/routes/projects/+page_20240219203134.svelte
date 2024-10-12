f8bc2dd6d3eac2824d35541f18aef04d18a71a52
Freezler <r.l.devries@live.nl>
2024-10-12 18:58:53 +0200

    fix: hero bg image

---
+++ .history/src/routes/projects/+page_20240219203134.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onDestroy, onMount } from 'svelte';
+	import LoginForm from '../../components/LoginForm.svelte';
+	import { fade } from 'svelte/transition';
+	onMount(() => console.log('Mounted Login page'));
+	onDestroy(() => console.log('Unmounted  page'));
+</script>
+<main  transition:fade={{ delay: 2, duration:600 }}
+class="flex h-screen flex-col items-center justify-center">
+	<LoginForm/>
+	<slot />
+</main>
